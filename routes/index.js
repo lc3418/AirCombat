2ca05833eeeb3c1cf64ca5fa228b2977247a5152
lc3418 <kaladiya@qq.com>
2019-04-19 03:04:34 -0400

    ./view
    frontend got better appearance

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 router.get('/regis', function(req,res) {
 	res.render('regis');
+})
+
+router.get('/login', function(req,res) {
+	res.render('login');
 })
 
 module.exports = router;
